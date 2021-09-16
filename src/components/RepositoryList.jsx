aec27d14b6d90220378c9f1b5e355feb7181e3e9
lucashthiele <lucasthiele0@outlook.com>
2021-09-15 22:59:06 -0300

    useEffect and GitHub API fetch

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,18 +1,28 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
-import '../styles/repositorys.scss'
+import "../styles/repositorys.scss";
+
+// API: https://api.github.com/users/lucashthiele/repos
 
 const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
+  name: "unform",
+  description: "Forms in React",
+  link: "https://github.com/unform/unform",
+};
 
-export function RepositoryList(){
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/lucashthiele/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
-
       <ul>
         <RepositoryItem repository={repository} />
         <RepositoryItem repository={repository} />
