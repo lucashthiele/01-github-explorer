7c4cb87bd63e1c87bae4d50baa097c5a4b53b1b2
lucashthiele <lucasthiele0@outlook.com>
2021-09-15 23:05:07 -0300

    mapping repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,14 +3,6 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import "../styles/repositorys.scss";
 
-// API: https://api.github.com/users/lucashthiele/repos
-
-const repository = {
-  name: "unform",
-  description: "Forms in React",
-  link: "https://github.com/unform/unform",
-};
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
@@ -24,10 +16,11 @@ export function RepositoryList() {
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => {
+          return (
+            <RepositoryItem repository={repository} key={repository.name} />
+          );
+        })}
       </ul>
     </section>
   );
